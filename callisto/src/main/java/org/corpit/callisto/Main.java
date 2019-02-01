eec9bb854ace75d942b6cd4ce3a8fb69e9b671a2
Peter Gaze <peter.gaze@team.telstra.com>
2019-02-02 09:36:55 +1100

    initial

---
+++ callisto/src/main/java/org/corpit/callisto/Main.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.corpit.callisto;
+
+/**
+ *
+ * @author c796944
+ */
+public class Main
+{
+   
+}
